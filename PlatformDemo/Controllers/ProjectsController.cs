f7ecfb7fb7f7c5973816ec473b50d2dc27eb6773
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2021-09-19 11:58:02 +1200

    create projects controller align with tickets controller

---
+++ PlatformDemo/Controllers/ProjectsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the projects.");
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Reading project #{id}.");
+        }
+
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Creating a project.");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Updating a project");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleting project #{id}.");
+        }
+
+    }
+}
