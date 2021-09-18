8aad8a72590d604a4152ee6c94f8bac92dce5d57
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2021-09-19 11:44:35 +1200

    Add project files.

---
+++ PlatformDemo/Controllers/TicketsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        [HttpGet]        
+        public IActionResult Get()
+        {
+            return Ok("Reading all the tickets.");
+        }
+
+        [HttpGet("{id}")]        
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Reading ticket #{id}.");
+        }
+
+        [HttpPost]        
+        public IActionResult Post()
+        {
+            return Ok("Creating a ticket.");
+        }
+
+        [HttpPut]        
+        public IActionResult Put()
+        {
+            return Ok("Updating a ticket");
+        }
+
+        [HttpDelete("{id}")]        
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleting ticket #{id}.");
+        }
+
+    }
+}
