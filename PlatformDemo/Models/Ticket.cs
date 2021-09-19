102b9ff4eb3ef0d9cb8001ee7c2398a64108ce5a
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2021-09-19 17:21:00 +1200

    model validation using data annotation

--- PlatformDemo/Models/Ticket.cs
+++ PlatformDemo/Models/Ticket.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace PlatformDemo.Models
 {
     public class Ticket
     {
-        public int TicketId { get; set; }
-        public int ProjectId { get; set; }
+        public int? TicketId { get; set; }
+        [Required]
+        public int? ProjectId { get; set; } //int? -> make it's default value to be null, otherwise will be 0
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
     }
