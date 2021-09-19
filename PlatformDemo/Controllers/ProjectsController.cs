f5a1dff7891c1d8bcd060002109a8f030d5d3962
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2021-09-19 12:10:38 +1200

    modle binding for primitive type using route and query

--- PlatformDemo/Controllers/ProjectsController.cs
+++ PlatformDemo/Controllers/ProjectsController.cs
@@ -18,11 +18,27 @@ namespace PlatformDemo.Controllers
 
 
         [HttpGet("{id}")]
-        public IActionResult GetById(int id)
+        public IActionResult GetById(int id) //id from route
         {
             return Ok($"Reading project #{id}.");
         }
 
+        /// <summary>
+        /// api/projects/{pid}/tickets?tid={tid}
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("/api/projects/{pid}/tickets")]
+        //api/project/45/tickets
+        //api/project/45/tickets?tid=123
+        public IActionResult GetProjectTIcket(int pId, [FromQuery] int tId) //[FromQuery] makes tId has to come from Query
+        {
+            if (tId == 0)
+            {
+                return Ok($"Reading all tickets belong to project {pId}");
+            }
+            return Ok($"Reading project {pId}, ticket #{tId}");
+        }
 
         [HttpPost]
         public IActionResult Post()
