380b02ffe6851b4c51f0430d49e266868ef61080
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2021-09-22 15:25:23 +1200

    decouple models and rename projects

---
+++ Core/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Models
+{
+	public class Project
+	{
+		public int ProjectId { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string Name { get; set; }
+		public List<Ticket> Tickets { get; set; }
+	}
+}
---
+++ Core/Models/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Models
+{
+	public class Ticket
+	{
+        public int? TicketId { get; set; }
+        [Required]
+        public int? ProjectId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [StringLength(50)]
+        public string Owner { get; set; }
+        public DateTime? ReportDate { get; set; }
+        public DateTime? DueDate { get; set; }
+		public Project Project { get; set; }
+	}
+}
