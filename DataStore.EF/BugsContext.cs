3362e8793f883405b180b6f0d60aa84bb4442294
Wenjun Xie <wenjun.xie@npdc.govt.nz>
2021-09-22 16:16:57 +1200

    install entity framework core

---
+++ DataStore.EF/BugsContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Core.Models;
+
+namespace DataStore.EF
+{
+	//context reprensent the database
+	public class BugsContext : DbContext
+	{
+		public BugsContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Project> Products { get; set; }
+		public DbSet<Ticket> Tickets{ get; set; }
+
+		/// <summary>
+		/// Create database schema 
+		/// </summary>
+		/// <param name="modelBuilder"></param>
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Project>()
+				.HasMany(p => p.Tickets)
+				.WithOne(t => t.Project)
+				.HasForeignKey(t => t.ProjectId);
+
+			//seeding
+			modelBuilder.Entity<Project>().HasData(
+				new Project() { ProjectId = 1, Name = "Project 1"},
+				new Project() { ProjectId = 2, Name = "Project 3"}
+				);
+
+			modelBuilder.Entity<Ticket>().HasData(
+				new Ticket() { TicketId = 1, Title = "Bug #1", ProjectId= 1 },
+				new Ticket() { TicketId = 2, Title = "Bug #2", ProjectId= 1 },
+				new Ticket() { TicketId = 3, Title = "Bug #3", ProjectId= 2 }
+				);
+		}
+	}
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataStore.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,20 +14,38 @@ namespace PlatformDemo
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		private readonly IWebHostEnvironment _env;
+
+		public Startup(IWebHostEnvironment env)
+		{
+            _env = env;
+		}
         public void ConfigureServices(IServiceCollection services)
         {
+			if (_env.IsDevelopment())
+			{
+
+                //Inject context to DI
+                services.AddDbContext<BugsContext>(options =>
+                {
+                    options.UseInMemoryDatabase("Bugs");
+                });
+			}
+
             //2. Add the controller middleware dependency and use the default behavior
             services.AddControllers();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BugsContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                //Create the in-memory database for dev environment
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated(); 
             }
 
             app.UseRouting();
